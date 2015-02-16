d7cba3541a583757aff03682f8cc009162bf27e8
Neil Ellis <neil@cazcade.com>
2015-02-16 14:56:01 +0000

    And that's it folks, the bare bones of a continuously backed up volume.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 if [ -f /tmp/.running ]
 then
     echo "Already running!!!"
@@ -6,7 +6,7 @@ then
 fi
 
 touch /tmp/.running
-trap "rm /tmp/.running ; sleep 10 " EXIT
+trap "rm /tmp/.running ; sleep 1 " EXIT
 
 s3_dir=${AWS_S3_LOCAL_MOUNT_POINT}
 local_dir=${SHARE_VOLUME}
@@ -15,7 +15,7 @@ upper=${SIMPLE_QUOTA_UPPER_MB}
 max_files=${SIMPLE_QUOTA_MAX_FILES}
 
 dir_size=$( du -sm "$local_dir" | cut -f1 )
-echo "$local_dir usage is currently ${dir_size}MB limit is ${lower}MB (hard limit ${upper}) MB"
+echo "$local_dir usage is currently ${dir_size}MB limit is ${lower}MB (hard limit ${upper}MB)"
 
 if [ ! -f ${local_dir}/.syncinit ]
 then
@@ -24,7 +24,7 @@ then
     touch ${local_dir}/.syncinit
 fi
 
-inotifywait -r ${local_dir}
+inotifywait -t 60 -r ${local_dir}
 
 changed=$(find $local_dir -newer ${local_dir}/.synclast -type f -not -path "${local_dir}/.sync*/*" | wc -l)
 
@@ -45,12 +45,14 @@ fi
 
 if (( ${dir_size} > ${lower} ))  ||  (( ${num_files} > ${max_files} ))
 then
+    echo "Quota exceeded"
     if [ ! -f ${local_dir}/.sync-quota-exceeded ]
     then
         touch ${local_dir}/.sync-quota-exceeded
     fi
     rm -rf ${local_dir}/.syncbackup || true
 else
+    echo "Within quota"
     if [ -f ${local_dir}/.sync-quota-exceeded ]
     then
         rm ${local_dir}/.sync-quota-exceeded
@@ -61,6 +63,7 @@ fi
 
 #When over upper quota, we delete the files created since we hit the lower quota
 while [ $dir_size -gt ${upper} ] ; do
+    echo "Purge files as upper limt exceeded"
     find $local_dir -type f -newer ${local_dir}/.sync-quota-exceeded -exec rm -f {} \;
     find $local_dir -type d -empty -delete
 done
