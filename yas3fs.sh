b5856cc56cd0e1ccf664cd4400df41d25cc890bd
Neil Ellis <neil@cazcade.com>
2015-02-16 09:03:27 +0000

    Initial work

--- yas3fs.sh
+++ yas3fs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 export S3_URL=$(echo "s3://${AWS_S3_BUCKET}/${AWS_S3_PATH}")
-exec yas3fs -df --mkdir  $S3_URL /usr/local/var
+exec yas3fs -df --mkdir  $S3_URL $AWS_S3_LOCAL_MOUNT_POINT
 
