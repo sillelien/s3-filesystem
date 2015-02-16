69414b166d568cad124c2291d17ce4803ba29fe4
Neil Ellis <neil@cazcade.com>
2015-02-16 09:01:30 +0000

    Initial work

--- yas3fs.sh
+++ yas3fs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
-export S3_URL=$(echo "s3://codeserverlocalvars/${GITHUB_USER}/${GITHUB_PROJECT}/${GITHUB_BRANCH}" | tr '-' '.')
+export S3_URL=$(echo "s3://${AWS_S3_BUCKET}/${AWS_S3_PATH}")
 exec yas3fs -df --mkdir  $S3_URL /usr/local/var
 
