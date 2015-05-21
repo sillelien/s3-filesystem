2c825dcbeb6c657c7937129f8f7c425ea0550f0b
neilellis <hello@neilellis.me>
2015-05-21 17:45:34 +0100

    Reverted to the much more useable version.

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,27 @@
-FROM vizzbuzz/base
+FROM phusion/baseimage:0.9.16
 
 ENV AWS_ACCESS_KEY_ID CHANGE_ME
 ENV AWS_SECRET_ACCESS_KEY CHANGE_ME
 ENV AWS_DEFAULT_REGION eu-west-1
 ENV AWS_S3_LOCAL_MOUNT_POINT /var/s3
 ENV AWS_S3_PATH /test
-ENV S3_MAX_CHECK_FREQUENCY 300
+
+ENV SHARE_VOLUME /usr/local/var
 
 ENV SIMPLE_QUOTA_MAX_FILES 100
 ENV SIMPLE_QUOTA_LOWER_MB 100
 ENV SIMPLE_QUOTA_UPPER_MB 1024
 
-#This goes to S3
-VOLUME /usr/local/var
-
+VOLUME ${SHARE_VOLUME}
 
 RUN apt-get update && apt-get -y install inotify-tools rsync fuse python-pip && pip install yas3fs awscli && \
     sed -i'' 's/^# *user_allow_other/user_allow_other/' /etc/fuse.conf && \
-    chmod a+r /etc/fuse.conf
+    chmod a+r /etc/fuse.conf && yas3fs -h
 
 ADD yas3fs.sh /etc/service/yas3fs/run
 ADD backup_daily.sh /etc/cron.daily/backup
 ADD backup_monthly.sh /etc/cron.monthly/backup
 ADD update.sh /etc/service/update/run
 RUN chmod 755 /etc/service/yas3fs/run /etc/cron.daily/backup /etc/cron.monthly/backup /etc/service/update/run
-RUN chown  app:app /usr/local/var
-RUN chown  app:app /usr/local/app
-RUN chown  app:app /usr/local/tmp
-RUN chown  app:app /usr/local/cache
 
 CMD ["/sbin/my_init"]
--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ touch /tmp/.running
 trap "rm /tmp/.running ; sleep 1 " EXIT
 
 s3_dir=${AWS_S3_LOCAL_MOUNT_POINT}
-local_dir=/usr/local/var
+local_dir=${SHARE_VOLUME}
 lower=${SIMPLE_QUOTA_LOWER_MB}
 upper=${SIMPLE_QUOTA_UPPER_MB}
 max_files=${SIMPLE_QUOTA_MAX_FILES}
@@ -17,22 +17,13 @@ max_files=${SIMPLE_QUOTA_MAX_FILES}
 dir_size=$( du -sm "$local_dir" | cut -f1 )
 echo "$local_dir usage is currently ${dir_size}MB limit is ${lower}MB (hard limit ${upper}MB)"
 
-
-while [ ! -d $s3_dir ]
-do
-    echo "Waiting for $s3_dir"
-    sleep 1
-done
-
-
 if [ ! -f ${local_dir}/.syncinit ]
 then
     echo "Copying from S3"
-    rsync -arv --delete-after --temp-dir=/tmp --ignore-times --whole-file --backup --backup-dir=${local_dir}/.syncbackrev  --exclude ".sync*"  ${s3_dir}/ ${local_dir}/
+    rsync -arv --delete-after --temp-dir=/tmp  --backup --backup-dir=${local_dir}/.syncbackrev  --exclude ".sync*"  ${s3_dir}/ ${local_dir}/
     touch ${local_dir}/.syncinit
 fi
 
-#No wait for any files to change
 inotifywait -e modify -e create -e move -e delete -e attrib -e close_write -t 3600 -r ${local_dir}
 
 
@@ -59,7 +50,7 @@ else
         rm ${local_dir}/.sync-quota-exceeded
     fi
     touch ${local_dir}/.synclast
-    rsync -arv  --delete --temp-dir=/tmp --checksum --timeout=60 --backup --backup-dir=${local_dir}/.syncbackup  --exclude ".sync*"   ${local_dir}/ ${s3_dir}/
+    rsync -arv  --delete --temp-dir=/tmp  --timeout=60 --backup --backup-dir=${local_dir}/.syncbackup  --exclude ".sync*"   ${local_dir}/ ${s3_dir}/
 fi
 
 #When over upper quota, we delete the files created since we hit the lower quota
@@ -68,6 +59,3 @@ while [ $dir_size -gt ${upper} ] ; do
     find $local_dir -type f -newer ${local_dir}/.sync-quota-exceeded -exec rm -f {} \;
     find $local_dir -type d -empty -delete
 done
-
-#We need to limit how often we update to s3
-sleep ${S3_MAX_CHECK_FREQUENCY}
